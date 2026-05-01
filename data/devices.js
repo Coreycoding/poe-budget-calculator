31b02c2ba83bf86bba98dd29db09d11d243752c1
Coreycoding <Coreyflynn07@gmail.com>
2026-04-30 20:01:57 -0400

    Add switch models and device wattages

--- data/devices.js
+++ data/devices.js
@@ -1,1 +1,18 @@
+// Switch models and PoE budgets
+const SWITCHES = {
 
+  // Cisco Catalyst
+"C9300-48P": { name: "Cisco C9300-48P", raw: 1440, ports: 48 }
+
+  // Cisco Industrial 
+"IE-3200": { name: "Cisco IE-3200", raw: 240, ports: 8 }
+};
+
+// Device models and wattages
+const DEVICES = {
+
+  // Axis
+  "AXIS P3267": { watts: 15.0, vendor: "Axis" },
+  "
+
+}:
